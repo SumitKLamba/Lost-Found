01a22f4c9d3d20fc5de7f12c71f0c397e7d93e02
Sumit Kumar <sumitskl1999@gmail.com>
2019-08-28 00:51:18 +0530

    adv stage

--- lib/lostInformCards.dart
+++ lib/lostInformCards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'onClickCards.dart';
 
 class LostInformCards extends StatelessWidget {
   @override
@@ -6,86 +7,93 @@ class LostInformCards extends StatelessWidget {
     return Container(
       child: Column(
         children: <Widget>[
-          Stack(
-            overflow: Overflow.visible,
-            children: <Widget>[
-              Container(
-                  width: 300,
-                  height: 220,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Color(0xFF00b9f5),
-                    boxShadow: [
-                      new BoxShadow(
-                          color: Color(0x4D000000),
-                          offset: new Offset(0.0, 5.0),
-                          blurRadius: 20.0,
-                          spreadRadius: 1)
-                    ],
-                  )),
-              Positioned(
-                top: 110,
-                left: 20,
-                child: Container(
-                  width: 200,
-                  height: 25,
-                  color: Colors.transparent,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "@Username",
-                      style: TextStyle(
-                          fontFamily: 'Raleway-Light',
-                          color: Colors.white70,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w900),
+          InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return OnClickCards();
+              }));
+            },
+            child: Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Container(
+                    width: 300,
+                    height: 220,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Color(0xFF00b9f5),
+                      boxShadow: [
+                        new BoxShadow(
+                            color: Color(0x4D000000),
+                            offset: new Offset(0.0, 5.0),
+                            blurRadius: 20.0,
+                            spreadRadius: 1)
+                      ],
+                    )),
+                Positioned(
+                  top: 110,
+                  left: 20,
+                  child: Container(
+                    width: 200,
+                    height: 25,
+                    color: Colors.transparent,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "@Username",
+                        style: TextStyle(
+                            fontFamily: 'Raleway-Light',
+                            color: Colors.white70,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w900),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Positioned(
-                top: 135,
-                left: 20,
-                child: Container(
-                  width: 200,
-                  height: 60,
-                  color: Colors.transparent,
-                  child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Wrap(
-                        children: <Widget>[
-                          Text(
-                            "Title of Random Ideal Post",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 26,
-                                fontWeight: FontWeight.w800),
-                          ),
-                        ],
-                        direction: Axis.horizontal,
-                      )),
-                ),
-              ),
-              Positioned(
-                left: -5.0,
-                child: Container(
-                  height: 105.0,
-                  width: 310.0,
-                  child: Text('Lost Item Image'),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.white,
-                    boxShadow: [
-                      new BoxShadow(
-                          color: Color(0x4D000000),
-                          offset: new Offset(0.0, 5.0),
-                          blurRadius: 20.0,
-                          spreadRadius: 1)
-                    ],
+                Positioned(
+                  top: 135,
+                  left: 20,
+                  child: Container(
+                    width: 200,
+                    height: 60,
+                    color: Colors.transparent,
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Wrap(
+                          children: <Widget>[
+                            Text(
+                              "Title of Random Ideal Post",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                          ],
+                          direction: Axis.horizontal,
+                        )),
                   ),
                 ),
-              ),
-            ],
+                Positioned(
+                  left: -5.0,
+                  child: Container(
+                    height: 105.0,
+                    width: 310.0,
+                    child: Text('Lost Item Image'),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white,
+                      boxShadow: [
+                        new BoxShadow(
+                            color: Color(0x4D000000),
+                            offset: new Offset(0.0, 5.0),
+                            blurRadius: 20.0,
+                            spreadRadius: 1)
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           SizedBox(height: 50)
         ],
---
+++ lib/onClickCards.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class OnClickCards extends StatefulWidget {
+  @override
+  _OnClickCardsState createState() => _OnClickCardsState();
+}
+
+class _OnClickCardsState extends State<OnClickCards> {
+  String title = "";
+  String desc = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFfff1ec),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF00b9f5),
+        title: Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Item Info',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 23,
+            ),
+          ),
+        ),
+      ),
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: 700,
+          width: 340,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: 25,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 200,
+                  width: 360,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF00b9f5),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextField(
+                maxLength: 40,
+                onChanged: (text) {
+                  title = text;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Object',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextField(
+                maxLines: 6,
+                onChanged: (text) {
+                  desc = text;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Description',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: RawMaterialButton(
+                  fillColor: Color(0xFF00b9f5),
+                  constraints: BoxConstraints(
+                    minWidth: 100,
+                    minHeight: 50,
+                  ),
+                  shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                  onPressed: (){},
+                  child: Text('Claim',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
