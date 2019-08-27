f2a03512a7d3bc975be3359834f82cabd165c86a
Sumit Kumar <sumitskl1999@gmail.com>
2019-08-28 00:12:02 +0530

    added desc

--- lib/onClickFAB.dart
+++ lib/onClickFAB.dart
@@ -7,6 +7,8 @@ class OnClickFAB extends StatefulWidget {
 
 class _OnClickFABState extends State<OnClickFAB> {
   String title = "";
+  String desc = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,35 +26,59 @@ class _OnClickFABState extends State<OnClickFAB> {
           ),
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: 25,),
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              height: 200,
-              width: 360,
-              decoration: BoxDecoration(
-                color: Color(0xFF00b9f5),
-                borderRadius: BorderRadius.circular(8),
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: 700,
+          width: 340,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: 25,
               ),
-            ),
-          ),
-          SizedBox(height: 40,),
-          Container(
-            width: 340,
-            child: TextField(
-              maxLength: 40,
-              onChanged: (text){
-                title = text;
-              },
-              decoration: InputDecoration(
-                labelText: 'Object',
-                border: OutlineInputBorder(),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 200,
+                  width: 360,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF00b9f5),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
               ),
-            ),
+              SizedBox(
+                height: 40,
+              ),
+              TextField(
+                maxLength: 40,
+                onChanged: (text) {
+                  title = text;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Object',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextField(
+                maxLines: 6,
+                onChanged: (text) {
+                  desc = text;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Description',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
